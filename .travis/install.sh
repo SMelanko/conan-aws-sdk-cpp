1bdb8d76bd43b194abb786030d91986da6ff069c
e.tiagniriadno <e.tiagniriadno@avantize.com>
2019-05-30 15:06:29 +0300

    Added commands for updating and upgrading ubuntu packages

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,6 +21,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
+    sudo apt-get update
+    sudo apt-get upgrade
     sudo apt-get install gcc-multilib
 fi
 
